1cbea6968f9792fcafc931a39976479c1d85a827
Trevor Witter <trevorgwitter@gmail.com>
2016-09-07 07:55:01 -0400

    Removed extra dataframe

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('/Users/twitter/Desktop/Python Projects/Sample Data/Sample_ECG5.txt', delimiter = "\t", index_col=0)
 frame = DataFrame(df)
 
-df.columns=['ecg', 'bp', 'time2']
-ECG = frame['ecg']
+frame.index.names = ['time']
+df.columns=['ecg', 'bp']
 
 #Low pass filter for ECG signal
 def butter_lowpass(cutoff, fs, order=5):
@@ -24,7 +24,7 @@ def butter_lowpass_filter(data, cutoff, fs, order=5):
     y = lfilter(b, a, data)
     return y
 
-Filtered_ECG = butter_lowpass_filter(ECG, 3.667, 1000.0, order=5)
+Filtered_ECG = butter_lowpass_filter(frame['ecg'], 3.667, 1000.0, order=5)
 
 # Filter requirements.
 order = 6
@@ -34,32 +34,31 @@ cutoff = 3.667  # desired cutoff frequency of the filter, Hz
 # Get the filter coefficients so we can check its frequency response.
 b, a = butter_lowpass(cutoff, fs, order)
 
-frame2 = DataFrame(df, columns=['ecg', 'bp', 'time2', 'filtered_ecg'])
-frame2['filtered_ecg'] = Filtered_ECG
+frame['filtered_ecg']= Filtered_ECG
+
 
 #QRS threshold 
-threshold = (np.max(frame2.filtered_ecg) - (np.max(frame2.filtered_ecg)-np.min(frame2.filtered_ecg))*.2)
+threshold = (np.max(frame.filtered_ecg) - (np.max(frame.filtered_ecg)-np.min(frame.filtered_ecg))*.2)
 
 #QRS detection
 ind = 0
 ind2 = 0
-hbindex = [frame2.index[0]]
+hbindex = [frame.index[0]]
 heartbeat = []
 RRindex = []
 hbs = [0]
 timestamp = []
 for x in Filtered_ECG:
     ind += 1
-    if x > threshold and Filtered_ECG[ind-1] > Filtered_ECG[(ind-1)-1] and Filtered_ECG[ind-1] > Filtered_ECG[(ind-1)+1]:  
+    if x > threshold and Filtered_ECG[ind-1] > Filtered_ECG[(ind-1)-1] and Filtered_ECG[ind-1] > Filtered_ECG[(ind-1)+1]:       #Threshold on frame ecg works
         ind2 += 1
         hbs.append(ind2)
         heartbeat.append(ind2)
-        hbindex.append(frame2.index[ind]) #timestamp for all detectected QRS in ECG
+        hbindex.append(frame.index[ind]) #timestamp for all detectected QRS in ECG
     else:
         heartbeat.append(ind2)
 
-frame2['heart_beat'] = heartbeat
-frame2.index.names = ['time']
+frame['heart_beat'] = heartbeat
 
 #Heart Rate
 RR = np.diff(hbindex)
@@ -72,88 +71,75 @@ sbp = []
 mbp = []
 dbp = []
 for x in hbs:
-    sbp.append(np.max(frame2[frame2.heart_beat == x].bp))
-    mbp.append(np.mean(frame2[frame2.heart_beat == x].bp))
-    dbp.append(np.min(frame2[frame2.heart_beat == x].bp))
+    sbp.append(np.max(frame[frame.heart_beat == x].bp))
+    mbp.append(np.mean(frame[frame.heart_beat == x].bp))
+    dbp.append(np.min(frame[frame.heart_beat == x].bp))
+
+print frame 
 
 #Output dataframe
-frame3 = DataFrame(hbs[1:], columns = ['hb'])
-frame3['time'] = hbindex[1:]
-frame3['RR'] = RR
-frame3['HR'] = HR
-frame3['sbp'] = sbp[1:]
-frame3['mbp'] = mbp[1:]
-frame3['dbp'] = dbp[1:]
+frame2 = DataFrame(hbs[1:], columns = ['hb'])
+frame2['time'] = hbindex[1:]
+frame2['RR'] = RR
+frame2['HR'] = HR
+frame2['sbp'] = sbp[1:]
+frame2['mbp'] = mbp[1:]
+frame2['dbp'] = dbp[1:]
 
 #Binning by SBP
 BPbin= []
-for y in frame3.sbp:
-    BPbin.append(int((y - min(frame3.sbp))/3))
-frame3['bin'] = BPbin
+for y in frame2.sbp:
+    BPbin.append(int((y - min(frame2.sbp))/3))
+frame2['bin'] = BPbin
+frame2 = frame2[:(len(frame2))-2] #removes trailing incomplete cardiac cycle
+print frame2
 
-#frame3['time'] = frame3.time - min(frame3.time)
-frame3 = frame3[:123] 
-print frame3
+groupedRR = frame2['RR'].groupby(frame2['bin'])
+RRarray = groupedRR.mean() 
 
-groupedRR = frame3['RR'].groupby(frame3['bin'])
-print groupedRR.mean()
-RRarray = [groupedRR.mean()]
+groupedSBP = frame2['sbp'].groupby(frame2['bin'])
+SBParray = np.asarray(groupedSBP.mean())
+print SBParray
 
-groupedHR = frame3['HR'].groupby(frame3['bin'])
-print groupedHR.mean()
-HRarray = [groupedHR.mean()]
+bin_weight = groupedSBP.size()/frame2['hb'].max()
+frame3 = frame2.mean()
 
-groupedSBP = frame3['sbp'].groupby(frame3['bin'])
-print groupedSBP.mean()
-SBParray = [groupedSBP.mean()]
-
-print "bin size", groupedSBP.size()
-bin_weight = groupedSBP.size()/123
-print "bin weight", bin_weight 
 
 #linear regression
-slope, intercept, r_value, p_value, std_err = linregress(SBParray, HRarray)
-print "slope", slope
-print "r squared", r_value**2
-print "p", p_value
-
+#RR vs SBP
 slope, intercept, r_value, p_value, std_err = linregress(SBParray, RRarray)
-print "slope", slope
-print "r squared", r_value**2
-print "p", p_value
+frame3['BRS slope'] = slope
+frame3['R^2'] = r_value**2
+print frame3
+bestfit = [(i*0.012020)+intercept for i in SBParray]
 
 #plots plots plots plots plots plots plots plots plots plots plots
 fig = plt.figure()
 
 #ECG plot
 ax1 = fig.add_subplot(2, 1, 1)
-plt.plot(frame2.index, frame2.filtered_ecg)
-plt.plot(frame3.time, frame2.filtered_ecg[frame3.time], linestyle=' ', marker='o')
+plt.plot(frame.index, frame.filtered_ecg)
+plt.plot(frame2.time, frame.filtered_ecg[frame2.time], linestyle=' ', marker='o')
 
 #blood pressure plot
 ax2 = fig.add_subplot(2, 1, 2)
 plt.xlabel('Time, s')
-plt.plot(frame2.index, frame2.bp)
-plt.plot(frame3.time, frame3.sbp)
-plt.plot(frame3.time, frame3.mbp)
-plt.plot(frame3.time, frame3.dbp)
+plt.plot(frame.index, frame.bp)
+plt.plot(frame2.time, frame2.sbp)
+plt.plot(frame2.time, frame2.mbp)
+plt.plot(frame2.time, frame2.dbp)
 plt.ylabel('Blood Pressure')
 fig2 = plt.figure()
 
-#correlation plots
-#HR vs SBP 
-ax3 = fig2.add_subplot(1, 1, 1)
-plt.plot(SBParray, HRarray, linestyle=' ', marker='.')
-plt.ylabel('Heart Rate')
-plt.xlabel('Systolic Blood Pressure')
-
+#correlation plot
 #RR vs SBP 
 fig3 = plt.figure()
 ax4 = fig3.add_subplot(1, 1, 1)
-plt.plot(SBParray, RRarray, linestyle=' ', marker='.') 
+plt.plot(SBParray, RRarray, linestyle=' ', marker='.', color='k') 
 plt.ylabel('RR interval')
 plt.xlabel('Systolic Blood Pressure')
 plt.plot(SBParray, bestfit, marker=' ', linestyle='--', color='k')
 plt.xlim(117, 144)
-plt.text(119, 1.08, r'$Slope = %s,\ R^2=%s' % (np.round(slope, decimals = 2), np.round(frame3['R^2'], decimals = 2)))
+plt.text(119, 1.08, r'$Slope = %s,\ R^2=%s$' % (np.round(slope, decimals = 2), np.round(frame3['R^2'], decimals = 2)))
+
 plt.show()
